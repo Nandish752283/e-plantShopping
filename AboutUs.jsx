d47acadf39f3d085799208709e25c107af8dc9f2
Nandish752283 <104014663+Nandish752283@users.noreply.github.com>
2026-03-22 15:49:55 +0100

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We provide beautiful indoor plants to make your home fresh and green.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
